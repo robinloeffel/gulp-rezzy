19bda7c959dcfa0cb9a4f86841dab74f57ffa0e6
Robin Löffel <hi@robinloeffel.ch>
2020-05-27 19:05:59 +0200

    switch from tabs to spaces

--- index.js
+++ index.js
@@ -9,69 +9,71 @@ const supportedFormats = new Set([ '.jpg', '.jpeg', '.png', '.webp', '.tiff', '.
 const pluginName = 'gulp-rezzy';
 
 module.exports = (versions = []) => {
-    const stream = new Transform({ objectMode: true });
+  const stream = new Transform({
+    objectMode: true
+  });
 
-    stream._transform = async (file, encoding, done) => {
-        if (file.isNull() || !versions.length) {
-            done(undefined, file);
-            return;
+  stream._transform = async (file, encoding, done) => {
+    if (file.isNull() || !versions.length) {
+      done(undefined, file);
+      return;
+    }
+
+    if (file.isStream()) {
+      done(new PluginError(pluginName, 'Streams aren\'t supported!'));
+      return;
+    }
+
+    if (!Array.isArray(versions)) {
+      stream.emit('error', new PluginError(pluginName, 'The configuration has to be an array!'));
+    }
+
+    if (!supportedFormats.has(file.extname.toLowerCase())) {
+      stream.emit('error', new PluginError(pluginName, `Can't resize ${file.extname} files!`, {
+        fileName: file.path
+      }));
+    }
+
+    try {
+      const promises = versions.map(async version => {
+        if (!version.suffix) {
+          stream.emit('error', new PluginError(pluginName, `${JSON.stringify(version)} does't include a suffix.`));
         }
 
-        if (file.isStream()) {
-            done(new PluginError(pluginName, 'Streams aren\'t supported!'));
-            return;
-        }
+        const image = sharp(file.contents);
+        image.resize({
+          width: version.width,
+          height: version.height,
+          fit: version.fit,
+          position: version.position
+        });
 
-        if (!Array.isArray(versions)) {
-            stream.emit('error', new PluginError(pluginName, 'The configuration has to be an array!'));
-        }
+        const buffer = await image.toBuffer();
+        const resized = new File({
+          cwd: file.cwd,
+          base: file.base,
+          path: file.path.replace(file.extname, '') + version.suffix + file.extname,
+          contents: buffer
+        });
 
-        if (!supportedFormats.has(file.extname.toLowerCase())) {
-            stream.emit('error', new PluginError(pluginName, `Can't resize ${file.extname} files!`, {
-                fileName: file.path
-            }));
-        }
+        return resized;
+      });
 
-        try {
-            const promises = versions.map(async version => {
-                if (!version.suffix) {
-                    stream.emit('error', new PluginError(pluginName, `${JSON.stringify(version)} does't include a suffix.`));
-                }
+      const images = await Promise.all(promises);
+      images.forEach(image => {
+        stream.push(image);
+        log(`${chalk.cyan(pluginName)}: created ${chalk.yellow(image.relative)}`);
+      });
 
-                const image = sharp(file.contents);
-                image.resize({
-                    width: version.width,
-                    height: version.height,
-                    fit: version.fit,
-                    position: version.position
-                });
+      log(`${chalk.cyan(pluginName)}: from ${chalk.yellow(file.relative)}`);
 
-                const buffer = await image.toBuffer();
-                const resized = new File({
-                    cwd: file.cwd,
-                    base: file.base,
-                    path: file.path.replace(file.extname, '') + version.suffix + file.extname,
-                    contents: buffer
-                });
+      done();
+    } catch (error) {
+      stream.emit('error', new PluginError(pluginName, error, {
+        fileName: file.path
+      }));
+    }
+  };
 
-                return resized;
-            });
-
-            const images = await Promise.all(promises);
-            images.forEach(image => {
-                stream.push(image);
-                log(`${chalk.cyan(pluginName)}: created ${chalk.yellow(image.relative)}`);
-            });
-
-            log(`${chalk.cyan(pluginName)}: from ${chalk.yellow(file.relative)}`);
-
-            done();
-        } catch (error) {
-            stream.emit('error', new PluginError(pluginName, error, {
-                fileName: file.path
-            }));
-        }
-    };
-
-    return stream;
+  return stream;
 };
--- test/gulpfile.js
+++ test/gulpfile.js
@@ -2,20 +2,20 @@ const gulp = require('gulp');
 const rezzy = require('..');
 
 gulp.task('default', () => gulp.src('src/*')
-        .pipe(rezzy([{
-            width: 500,
-            suffix: '-500w'
-        }, {
-            height: 1000,
-            suffix: '-1000h'
-        }, {
-            width: 400,
-            height: 300,
-            suffix: '-400x300'
-        }, {
-            width: 400,
-            height: 300,
-            position: 'entropy',
-            suffix: '-entropy'
-        }]))
-        .pipe(gulp.dest('dist')));
+  .pipe(rezzy([{
+    width: 500,
+    suffix: '-500w'
+  }, {
+    height: 1000,
+    suffix: '-1000h'
+  }, {
+    width: 400,
+    height: 300,
+    suffix: '-400x300'
+  }, {
+    width: 400,
+    height: 300,
+    position: 'entropy',
+    suffix: '-entropy'
+  }]))
+  .pipe(gulp.dest('dist')));
