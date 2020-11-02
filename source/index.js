2d32fcf8bd6ad3f45e316606dfc171491982eae8
Robin Löffel <hi@robinloeffel.ch>
2020-11-02 10:44:02 +0100

    directly return the transform stream

--- source/index.js
+++ source/index.js
@@ -2,38 +2,35 @@ const { Transform } = require('stream');
 const sharp = require('sharp');
 const log = require('fancy-log');
 const chalk = require('chalk');
-const File = require('vinyl');
+const Vinyl = require('vinyl');
 const PluginError = require('plugin-error');
 
 const supportedFormats = new Set([ '.jpg', '.jpeg', '.png', '.webp', '.tiff', '.raw' ]);
 const pluginName = 'gulp-rezzy';
 
-module.exports = (versions = []) => {
-  const stream = new Transform({
-    objectMode: true
-  });
-
-  stream._transform = async (file, _encoding, done) => {
+module.exports = (versions = []) => new Transform({
+  objectMode: true,
+  async transform(file, _encoding, done) {
     if (file.isNull() || versions.length === 0) {
       return done(null, file);
     }
 
     if (file.isStream()) {
-      return stream.emit('error', new PluginError(pluginName, 'Streams are not supported!'));
+      return this.emit('error', new PluginError(pluginName, 'Streams are not supported!'));
     }
 
     if (!Array.isArray(versions)) {
-      return stream.emit('error', new PluginError(pluginName, 'The options must be an array'));
+      return this.emit('error', new PluginError(pluginName, 'The options must be an array'));
     }
 
     if (!supportedFormats.has(file.extname.toLowerCase())) {
-      return stream.emit('error', new PluginError(pluginName, `Can't resize ${file.extname} files!`));
+      return this.emit('error', new PluginError(pluginName, `Can't resize ${file.extname} files!`));
     }
 
     try {
       const promises = versions.map(async version => {
         if (!version.suffix) {
-          return stream.emit('error', new PluginError(pluginName, `${JSON.stringify(version)} does't include a suffix.`));
+          return this.emit('error', new PluginError(pluginName, `${JSON.stringify(version)} does't include a suffix.`));
         }
 
         const image = sharp(file.contents);
@@ -45,7 +42,7 @@ module.exports = (versions = []) => {
         });
 
         const buffer = await image.toBuffer();
-        const resized = new File({
+        const resized = new Vinyl({
           cwd: file.cwd,
           base: file.base,
           path: file.path.replace(file.extname, '') + version.suffix + file.extname,
@@ -57,16 +54,14 @@ module.exports = (versions = []) => {
 
       const images = await Promise.all(promises);
       images.forEach(image => {
-        stream.push(image);
+        this.push(image);
         log(`${chalk.cyan(pluginName)}: created ${chalk.yellow(image.relative)}`);
       });
-      log(`${chalk.cyan(pluginName)}: from ${chalk.yellow(file.relative)}`);
 
       return done();
     } catch (error) {
-      return stream.emit('error', new PluginError(pluginName, error));
+      return this.emit('error', new PluginError(pluginName, error));
     }
-  };
+  }
+});
 
-  return stream;
-};
